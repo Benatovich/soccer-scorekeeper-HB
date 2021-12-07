a7c4e0297fd405cedcc0c239e681d0e47bf0506b
Benatovich <sambenatovich@gmail.com>
2021-12-06 19:23:25 -0500

    if I use the  method, the tests pass for any input

--- test/example.test.js
+++ test/example.test.js
@@ -9,7 +9,7 @@ const test = QUnit.test;
 test('renderTeam should create teamDiv, fill it with nameDiv and ScoreDiv, and populate them with the name and score respectively', (expect) => {
     //Arrange
     // Set up your arguments and expectations
-    const expected = teamDiv.append('Sam', 8);
+    const expected = '<div class="team"></div>';
     
     //Act 
     // Call the function you're testing and set the result to a const
@@ -18,6 +18,19 @@ test('renderTeam should create teamDiv, fill it with nameDiv and ScoreDiv, and p
     //Expect
     // Make assertions about what is expected versus the actual result
     expect.equal(actual, expected, 'should return a team named Sam with 8 goals');
+});
+test('renderTeam should create teamDiv, fill it with nameDiv and ScoreDiv, and populate them with the name and score respectively', (expect) => {
+    //Arrange
+    // Set up your arguments and expectations
+    const expected = '[object HTMLDivElement]';
+    
+    //Act 
+    // Call the function you're testing and set the result to a const
+    const actual = renderTeam('Sam', 8);
+
+    //Expect
+    // Make assertions about what is expected versus the actual result
+    expect.equal(`${actual}`, `${expected}`, 'should return a team named Sam with 8 goals');
 });
 
 test('renderGame should generate stats for the current game', (expect) => {
